7257de4c6433f28e2626e228efb30099c098b43a
shivup5005 <shivup5005@gmail.com>
2024-01-24 15:37:37 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const list = ["name", "age", "photo", "photos"];
+  const [filteredData, setFilteredData] = useState(false);
 
-  const list = ['name', 'age', 'photo', 'photos']
-  const [filteredData, setFilteredData] = useState(false)
+  const handleChange = (e) => {
+    const data = list.filter((ele) => {
+      if (ele.toLowerCase().includes(e.target.value.trim().toLowerCase())) {
+        return ele;
+      }
+    });
 
-  const handleChange=(e)=>{
-    const data = list.filter((ele)=>{
-      if(ele.toLowerCase().includes(e.target.value.trim().toLowerCase())){
-        return ele
-      } 
-    })
+    setFilteredData(data);
+  };
 
-    setFilteredData(data)
-  }
- 
   return (
     <>
-    <input type='text' onChange={handleChange}/>
-    {
-      filteredData && filteredData.map((ele,i)=>{
-        return <div key={i}>{ele}</div>
-      })
-    }
- 
+      <input type="text" onChange={handleChange} />
+      {filteredData &&
+        filteredData.map((ele, i) => {
+          return <div key={i}>{ele}</div>;
+        })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
