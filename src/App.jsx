2f0119d319aed33d183dfe2f5a2fd60dea3db53b
shivup5005 <shivup5005@gmail.com>
2024-01-24 15:08:42 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const list = ['name', 'age', 'photo', 'photos']
+  const [filteredData, setFilteredData] = useState(false)
+
+  const handleChange=(e)=>{
+    const data = list.filter((ele)=>{
+      if(ele.toLowerCase().includes(e.target.value.trim().toLowerCase())){
+        return ele
+      } 
+    })
+
+    setFilteredData(data)
+  }
  
   return (
     <>
-    <h1>hello world</h1>
+    <input type='text' onChange={handleChange}/>
+    {
+      filteredData && filteredData.map((ele,i)=>{
+        return <div key={i}>{ele}</div>
+      })
+    }
  
     </>
   )
