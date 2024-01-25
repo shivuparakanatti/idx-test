c5b327f63ac4b46d6c84748671a753556f24d7b9
shivup5005 <shivup5005@gmail.com>
2024-01-25 18:29:00 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,7 @@ import "./App.css";
 
 function App() {
  const[count,setCount] = useState(0) 
- const[isEven,setIsEven] = useState('no') 
-  const handleChange = (e) => {
-    if(count<100000000000){
 
-      setCount(count+1)
-    }
-  };
-
-  const handleMinus = ()=>{
-    setCount(count-1)
-  }
-
-  const isEvenMemo = useMemo(()=>{
-    if(count%2===0){
-      setIsEven('yes')
-    }
-  })
  
 
   return (
